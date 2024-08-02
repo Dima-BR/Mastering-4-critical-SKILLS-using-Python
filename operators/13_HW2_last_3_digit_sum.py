8f466f532bf4e4b85c51822eb75731d830e249b8
Dima-Rawashdeh <44325929+Dima-BR@users.noreply.github.com>
2024-08-02 13:33:43 +0300

    Update 13_HW2_last_3_digit_sum.py

--- operators/13_HW2_last_3_digit_sum.py
+++ operators/13_HW2_last_3_digit_sum.py
@@ -1,3 +1,9 @@
+# Homework 2: Last 3 digits sum
+# ● Write a program that reads an integer and prints the sum of its last 3 digits.
+# ● Inputs ⇒ Outputs examples ○ 15 ⇒6
+# ○ 125 ⇒8 ○ 1000 ⇒0 ○ 1001 ⇒1 ○ 1234 ⇒9 ○ 99999⇒27
+
+
 n = int(input("Please enter the number"))
 
 l1 = n%10
